24c40123e6948471bb151bb43af64cdc3f7fd6fc
arathi <arathi@qburst.com>
2018-08-29 18:23:42 +0530

    add-or-edit-project-view-design #160084503

--- src/app/model/project-model.ts
+++ src/app/model/project-model.ts
@@ -2,4 +2,9 @@ export class Project {
     members:string[];
     numberOfMembers: Number;
     name: string;
+    projectdescription:string;
+    constructor(name:string, projectdescription:string) {
+        this.name = name;
+        this.projectdescription = projectdescription;
+    }
 }
--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Project } from "../model/project-model";
-import { Member } from "../model/member-model";
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ProjectmemberService } from "../service/projectmember.service";
+import { Project } from "../model/project-model"
+import { ProjectService } from "../service/project.service";
+import { ProjectMember } from '../model/ProjectMembers';
 
 @Component({
   selector: 'app-project',
@@ -9,9 +11,129 @@ import { Member } from "../model/member-model";
 })
 export class ProjectComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('des') des: ElementRef;
+  @ViewChild('email') email: ElementRef;
+  
+  projectmembers:ProjectMember[];
+  projectmember:ProjectMember;
+
+  public project:Project;
+  count:number;
+
+  public show1:boolean = true;
+  public show2:boolean = true;
+
+  constructor(private projectmemberservice: ProjectmemberService, private projectservice: ProjectService) {  }
+  
 
   ngOnInit() {
+
+    this.count = 0;
+    this.getProjectMembers();
   }
 
+  changeVisibility1() {
+    this.show1 = !this.show1;
+  }
+
+  setpromem(){
+    if(this.count == 0)
+      this.changeVisibility2();
+    else
+      this.addProjectMember();
+  }
+  changeVisibility2() {
+    this.show2 = !this.show2;
+    this.count++;
+  }
+
+  set(): void {
+
+    var proname = (<HTMLInputElement>document.getElementById("projectname")).value;
+    var prodesc = this.des.nativeElement.innerText;
+
+    this.project = new Project(proname, prodesc);
+    this.projectservice.addProject(this.project).subscribe(pro => {});
+
+    console.log(this.project.name);
+    console.log(this.project.projectdescription);
+
+  }
+
+  getProjectMembers(): void {
+      this.projectmemberservice.getProjectMembers()
+    .subscribe(projectmembers => this.projectmembers = projectmembers);
+  }
+
+  addProjectMember(): void {
+
+    var email = (<HTMLInputElement>document.getElementById("emailvalue")).value;
+    var role = (<HTMLInputElement>document.getElementById("rolevalue")).value;
+
+    if(email!='' || role!=''){
+    this.projectmember = new ProjectMember(email, role);
+
+    if (!this.projectmember) { return; }
+    this.projectmemberservice.addProjectMember(this.projectmember)
+      .subscribe(projectmember => {
+        this.projectmembers.push(projectmember);
+      });
+
+      this.getProjectMembers();
+
+      (<HTMLInputElement>document.getElementById("emailvalue")).value = null;
+      (<HTMLInputElement>document.getElementById("rolevalue")).value = "None";
+    }
+  }
+
+  delete(projectmember:ProjectMember): void {
+    this.projectmembers = this.projectmembers.filter(h => h !== projectmember);
+    this.projectmemberservice.deleteProjectMember(projectmember).subscribe();
+  }
+
+  cancel(): void {
+    (<HTMLInputElement>document.getElementById("projectname")).value = null;
+    if(this.show1 != true){
+    (<HTMLInputElement>document.getElementById("projectdescription")).value = null;
+    }
+    (<HTMLInputElement>document.getElementById("emailvalue")).value = null;
+    (<HTMLInputElement>document.getElementById("rolevalue")).value = "None";
+
+    this.show1 = true;
+    this.show2 = true;
+    this.count--;
+
+    for ( let promem of this.projectmembers) { 
+      this.delete(promem);
+   }
+  }
+  addproject(): void{
+    this.getProjectMembers()
+
+    this.set();
+
+
+    console.log(this.projectmembers);
+    // this.addProjectMember();
+  }
+  change(projectmember:ProjectMember): void{
+
+    var id = projectmember.id;
+
+    var emailaddress = (<HTMLInputElement>document.getElementById("email"+id.toString())).value;
+    var memberole = (<HTMLInputElement>document.getElementById("role"+id.toString())).value;
+    
+    console.log(emailaddress);
+
+    if(emailaddress!='' && memberole!=''){
+      projectmember = new ProjectMember(emailaddress, memberole);
+    }
+
+    projectmember.id = id;
+
+    this.projectmemberservice.updateProjectMember(projectmember).subscribe();
+
+    this.getProjectMembers()
+
+  }
 }
