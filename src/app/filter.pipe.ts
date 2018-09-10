4216fe567945e1db53abb451b6078e0abec1af4b
arathi <arathi@qburst.com>
2018-09-10 14:15:54 +0530

    10th aug afternoon

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,16 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { MemberTask } from './model/task-model';
+import { Member } from './model/member-model';
 
 @Pipe({
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
-  transform(items: MemberTask[], searchText: string): any[] {
+  transform(items: Member[], searchText: string): any[] {
     if(!items) return [];
     if(!searchText) return items;
 searchText = searchText.toLowerCase();
 return items.filter( it => {
-      return it.member_name.toLowerCase().includes(searchText);
+      return it.name.toLowerCase().includes(searchText);
     });
    }
 }
--- src/app/userslist/userslist.component.ts
+++ src/app/userslist/userslist.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MemberTaskService } from '../service/member-task.service'
-import { MemberTask } from '../model/task-model';
+import { AdminviewallserviceService } from '../service/adminviewallservice.service';
+import { Member } from '../model/member-model';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,23 +11,31 @@ import { Router } from '@angular/router';
 })
 export class UserslistComponent implements OnInit {
 
-  public members: MemberTask[];
+  public members: Member[];
+  memberArray: Member[];
 
-  constructor(private membertaskservice : MemberTaskService, public router: Router) { }
+  constructor(private membertaskservice : MemberTaskService, public router: Router, private viewallservice: AdminviewallserviceService) { }
 
   ngOnInit() {
-    // console.log("this is a hi")
-    this.members = this.membertaskservice.getMember1();
-    // console.log(this.members);
+    console.log("this is a hi")
+    this.viewallservice.getMembers()
+    .subscribe(membersArr => this.getMembers(membersArr));
+  }
+
+  getMembers(membersArr): void {
+    this.members = membersArr;
+    console.log("hiii");
+    console.log(this.members);
   }
 
   gotoDailyStatus(membername:string) {
     console.log(membername);
     this.router.navigateByUrl('/daily-status');
   }
-  // changeCSS(){
-  //   document.getElementById("search_name").setAttribute("class","searching_name")
-  //   document.getElementById("member").style.visibility = "hidden";
-  // }
-
+  changeCSS(){
+    document.getElementById("search_name").style.display = "flex";
+    document.getElementById("userslist").style.display = "block";
+    for(var i in this.members)
+    document.getElementById("member_name"+i).style.display = "flex";
+  }
 }
