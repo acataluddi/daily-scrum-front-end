3016a845339eea2e5bcf933d6ede4096193ee0fc
athiramohandas <athiramohandas@qburst.com>
2018-09-14 14:59:27 +0530

    [#160409378] Authentication to API

--- src/app/status.service.ts
+++ src/app/status.service.ts
@@ -20,25 +20,15 @@ export class AuthGuard implements CanActivate {
 console.log("loginfrewgwtre:");
 console.log(route.url[0].path);
 
-    // this.socialAuthService.authState.subscribe((user) => {
-    //     console.log("user:");
-    //     console.log(user);
-    //     this.user = user;
-    //     if (user != null) {
-    //       this.loginservice.loginMember(user.idToken)
-    //         .subscribe(msg => {
-    //           msg.userType;
-    //           if (msg.userType === "Admin" && msg.userType === "Manager") {
-    //             this.flag = true;
-    //             // console.log("flag:"+this.flag);
-    //             // this.router.navigate(['/dashboard']);
-    //             // return true;
-    //           }
-  
-    //         });
-    //     }
-    //     });
-        if (localStorage.getItem("logged") == 'true') {
+    this.socialAuthService.authState.subscribe((user) => {
+        console.log("user:");
+        console.log(user);
+        this.user = user;
+        if (user != null) {
+                this.flag = true;
+        }
+        });
+        if (this.flag == true) {
             if (route.url[0].path === 'admin-view-all'){
                 this.socialAuthService.authState.subscribe((user) => {
                     console.log("user:");
