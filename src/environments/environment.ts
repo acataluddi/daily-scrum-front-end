af1f4fba8a882e0bb09c1c54549489469cc98e48
neerajdixon1996 <neerajd@qburst.com>
2018-10-11 09:45:10 +0530

    Completed with scroll

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBase: 'http://10.4.6.71:8080/DailyScrum',
+  apiBase: 'http://10.4.6.58:8080/DailyScrum',
   loginURL: 'http://localhost:4200/login'
 };
 
